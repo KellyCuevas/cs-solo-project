312578dc24b7684ea27bf5102fe366aab537130d
kcuevas <cuev73@live.com>
2022-10-19 23:01:56 -0400

    added route handlers for wine colors

--- server/controllers/wineCardController.js
+++ server/controllers/wineCardController.js
@@ -25,6 +25,70 @@ wineCardController.getWine = async(req, res, next) => {
    };
 };
 
+wineCardController.getWineRed = async(req, res, next) => {
+  console.log('this is wineCardController.getWine');
+  try{
+    const wineData = await models.Wines.find({wine_color: ['red', 'Red']});
+    console.log('this is wineData' + wineData);
+    res.locals.wineList = wineData;
+    return next();
+  }catch(err) {
+    return next({
+      log: 'error in wineController.getWine',
+      status: 204,
+      message: { err: 'WineList not available' },
+    });
+   };
+};
+
+wineCardController.getWineWhite = async(req, res, next) => {
+  console.log('this is wineCardController.getWine');
+  try{
+    const wineData = await models.Wines.find({wine_color: ['white', 'White']});
+    console.log('this is wineData' + wineData);
+    res.locals.wineList = wineData;
+    return next();
+  }catch(err) {
+    return next({
+      log: 'error in wineController.getWine',
+      status: 204,
+      message: { err: 'WineList not available' },
+    });
+   };
+};
+
+wineCardController.getWineRose = async(req, res, next) => {
+  console.log('this is wineCardController.getWine');
+  try{
+    const wineData = await models.Wines.find({wine_color: ['rose', 'Rose', 'pink', 'Pink']});
+    console.log('this is wineData' + wineData);
+    res.locals.wineList = wineData;
+    return next();
+  }catch(err) {
+    return next({
+      log: 'error in wineController.getWine',
+      status: 204,
+      message: { err: 'WineList not available' },
+    });
+   };
+};
+
+wineCardController.getWineSparkling = async(req, res, next) => {
+  console.log('this is wineCardController.getWine');
+  try{
+    const wineData = await models.Wines.find({wine_color: ['sparkling', 'Sparkling', 'champagne', 'Champagne']});
+    console.log('this is wineData' + wineData);
+    res.locals.wineList = wineData;
+    return next();
+  }catch(err) {
+    return next({
+      log: 'error in wineController.getWine',
+      status: 204,
+      message: { err: 'WineList not available' },
+    });
+   };
+};
+
 wineCardController.addWine = async(req, res, next) => {
   // const {name, wine_color, varietal, winery_location, vintage_year, price, tasting_notes} = req.body;
   console.log(req.body.name);
